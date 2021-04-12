d30d3fe431f7e4a10e0d27d3a216c85bb3254d3d
Jason Buckner <jason@jasonbuckner.com>
2021-04-12 10:47:13 -0700

    Add return type to addSeconds

--- src/add-seconds.ts
+++ src/add-seconds.ts
@@ -6,9 +6,9 @@ import { Seconds } from './models';
  * @export
  * @param {Date} date
  * @param {Seconds} seconds
- * @returns
+ * @returns {Date}
  */
-export function addSeconds(date: Date, seconds: Seconds) {
+export function addSeconds(date: Date, seconds: Seconds): Date {
   // we're adding seconds, but want millisecond accuracy so use milliseconds
   date.setMilliseconds(date.getMilliseconds() + seconds * 1000);
   return date;
